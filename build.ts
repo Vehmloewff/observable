5b9ef1fa88442e5c940c31df91698580a763b647
Elijah Mooring <vehmloewff@gmail.com>
2025-09-03 18:17:17 -0700

    feat: we should not bundle peer dependencies

--- build.ts
+++ build.ts
@@ -1,9 +1,11 @@
 import { build } from "bun";
 import dts from "bun-plugin-dts";
+import { peerDependencies } from "./package.json";
 
 await build({
 	entrypoints: ["./index.ts"],
 	outdir: "./dist",
 	plugins: [dts()],
+	external: Object.keys(peerDependencies),
 	format: "esm",
 });
